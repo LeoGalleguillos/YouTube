37db18d232fa62dcfa60267c9f83170329769edb
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-01-24 16:11:14 +0000

    Write preliminary app and channel entity classes

---
+++ src/Model/Entity/App.php
@@ -0,0 +1,11 @@
+<?php
+namespace LeoGalleguillos\YouTube\Model\Entity;
+
+class App
+{
+    public $appId;
+    public $nameId;
+    public $oAuth2ClientId;
+    public $oAuth2ClientSecret;
+
+}
---
+++ src/Model/Entity/Channel.php
@@ -0,0 +1,9 @@
+<?php
+namespace LeoGalleguillos\YouTube\Model\Entity;
+
+class Channel
+{
+    public $channelId;
+    public $userId;
+    public $name;
+}
