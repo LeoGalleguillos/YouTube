0285fbd89cd723f188b530ef32f79204151cc439
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-03-09 11:08:38 +0000

    Write and test table model for app_channel table

---
+++ src/Model/Table/AppChannel.php
@@ -0,0 +1,59 @@
+<?php
+namespace LeoGalleguillos\YouTube\Model\Table;
+
+use DateTime;
+use Generator;
+use Zend\Db\Adapter\Adapter;
+
+class AppChannel
+{
+    /**
+     * @var Adapter
+     */
+    protected $adapter;
+
+    public function __construct(
+        Adapter $adapter
+    ) {
+        $this->adapter   = $adapter;
+    }
+
+    public function insertOnDuplicateKeyUpdate(
+        int $appId,
+        int $channelId,
+        string $accessToken,
+        DateTime $accessTokenExpiration,
+        string $refreshToken
+    ): int {
+        $sql = '
+            INSERT
+              INTO `app_channel` (
+                       `app_id`
+                     , `channel_id`
+                     , `access_token`
+                     , `access_token_expiration`
+                     , `refresh_token`
+                   )
+            VALUES (?, ?, ?, ?, ?)
+                ON DUPLICATE KEY UPDATE
+                   `access_token` = ?
+                 , `access_token_expiration` = ?
+                 , `refresh_token` = ?
+                 ;
+        ';
+        $parameters = [
+            $appId,
+            $channelId,
+            $accessToken,
+            $accessTokenExpiration->format('Y-m-d h:i:s'),
+            $refreshToken,
+            $accessToken,
+            $accessTokenExpiration->format('Y-m-d h:i:s'),
+            $refreshToken,
+        ];
+        return $this->adapter
+                    ->query($sql)
+                    ->execute($parameters)
+                    ->getAffectedRows();
+    }
+}
---
+++ test/Model/Table/AppChannelTest.php
@@ -0,0 +1,34 @@
+<?php
+namespace LeoGalleguillos\AmazonYouTubeTest\Model\Table;
+
+use DateTime;
+use LeoGalleguillos\YouTube\Model\Table as YouTubeTable;
+use LeoGalleguillos\Test\TableTestCase;
+
+class AppChannelTest extends TableTestCase
+{
+    protected function setUp()
+    {
+        $this->appChannelTable = new YouTubeTable\AppChannel(
+            $this->getAdapter()
+        );
+
+        $this->dropTable('app_channel');
+        $this->createTable('app_channel');
+    }
+
+    public function testInsertOnDuplicateKeyUpdate()
+    {
+        $affectedRows = $this->appChannelTable->insertOnDuplicateKeyUpdate(
+            1,
+            2,
+            'access_token',
+            new DateTime(),
+            'refresh_token'
+        );
+        $this->assertSame(
+            1,
+            $affectedRows
+        );
+    }
+}
