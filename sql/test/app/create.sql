85fde4e5ff940b5ed2c34775fd0f60b0ece5a45f
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-03-09 10:53:59 +0000

    Write sql files for app table

---
+++ sql/test/app/create.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `app` (
+  `app_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`app_id`)
+) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
