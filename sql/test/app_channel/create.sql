5749d136885dd089ffc02629c5c5afdaac2b5d9e
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-03-09 10:57:51 +0000

    Write sql files for app_channel table

---
+++ sql/test/app_channel/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `app_channel` (
+  `app_channel_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `app_id` int(10) unsigned not null,
+  `channel_id` int(10) unsigned not null,
+  `access_token` varchar(255) DEFAULT NULL,
+  `access_token_expiration` DATETIME DEFAULT NULL,
+  `refresh_token` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`app_channel_id`)
+) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
